8b4c24e96a62e46d78b2bdeef5a249a55e4f3d26
Emily Leones <eleones@thoughtworks.com>
2018-09-08 01:06:16 +0200

    add tooltip hide/show logix

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,8 +1,31 @@
-const priceDiv: HTMLElement | null = document.getElementById('zeitgeistBadge_feature_div');
-console.log(priceDiv);
-const ratingDiv: HTMLElement = document.createElement('div');
-ratingDiv.id = 'unbeadable';
 const ratingImage: HTMLElement = document.createElement('img');
 ratingImage.setAttribute('src', 'https://78.media.tumblr.com/avatar_f27f604004a0_128.pnj');
+
+// send request to backend
+const toolTip: HTMLElement = document.createElement('div');
+toolTip.id = 'unbeadable-tooltip';
+toolTip.style.visibility = "hidden";
+
+
+const showToolTipHandler = (toolTip: HTMLElement) => () => {
+    if(toolTip.style.visibility == "hidden") {
+        console.log("ToolTip Visible");
+        toolTip.style.visibility = "visible";
+    }
+};
+
+const hideToolTipHandler = (toolTip: HTMLElement) => () => {
+    if(toolTip.style.visibility == "visible") {
+        console.log("ToolTip Hidden");
+        toolTip.style.visibility = "hidden";
+    }
+};
+
+const ratingDiv: HTMLElement = document.createElement('div');
+ratingDiv.id = 'unbeadable';
 ratingDiv.appendChild(ratingImage);
+ratingDiv.onmouseover = showToolTipHandler(toolTip);
+ratingDiv.onmouseout = hideToolTipHandler(toolTip);
+
+const priceDiv: HTMLElement | null = document.getElementById('zeitgeistBadge_feature_div');
 priceDiv!.appendChild(ratingDiv);
