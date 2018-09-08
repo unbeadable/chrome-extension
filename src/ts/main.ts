379dfad3d272973964c265bd83e993c7d2908f29
Emily Leones <eleones@thoughtworks.com>
2018-09-08 05:32:34 +0200

    prettifying tooltip

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -32,11 +32,18 @@ badgeText.setAttribute('style', '' +
     '    line-height: normal;');
 badgeText.innerText = 'This product contributes to the pollution of our oceans.';
 
-const tooltipContent = document.createElement('h2');
-tooltipContent.innerText = 'Help us keep the oceans clean!';
+const subline = document.createElement('h2');
+subline.innerText = 'Unbead the oceans.';
 
 const tooltipImage = document.createElement('img');
 tooltipImage.setAttribute('src', 'https://media.giphy.com/media/l4FGtZqkEJnw3V2lW/giphy.gif');
+
+const tooltipTitle = document.createElement('p');
+tooltipTitle.setAttribute('style', 'font-weight: bold;');
+tooltipTitle.innerText = 'This product contains Microplastic!';
+
+const tooltipDescription = document.createElement('p');
+tooltipDescription.innerText = 'Microplastic pollution affects on sea life in numerous ways. It destroys ecosystems, changes the behaviour of species in the sea by affecting their brain and increases mortality rates of the species.';
 
 const tooltipSpan: HTMLElement = document.createElement('span');
 tooltipSpan.setAttribute('class', 'tooltiptext');
@@ -54,8 +61,10 @@ tooltipSpan.setAttribute('style', 'visibility: hidden;\n' +
     '    margin-top: 5px;\n' +
     '    margin-left: -125px;\n' +
     '    box-shadow: 0 1px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);');
+tooltipSpan.appendChild(tooltipTitle);
+tooltipSpan.appendChild(tooltipDescription);
 tooltipSpan.appendChild(tooltipImage);
-tooltipSpan.appendChild(tooltipContent);
+tooltipSpan.appendChild(subline);
 
 const asin = findAsin();
 if (!!asin) {
