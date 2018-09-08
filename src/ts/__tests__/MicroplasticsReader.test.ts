2278498c35f8155e94888754bc50c54b2acb5e79
bstein <bstein@thoughtworks.com>
2018-09-08 09:46:32 +0200

    fix tests

--- src/ts/__tests__/MicroplasticsReader.test.ts
+++ src/ts/__tests__/MicroplasticsReader.test.ts
@@ -2,8 +2,7 @@ import {MicroplasticAsinLookup} from '../MicroplasticAsinLookup';
 
 describe('MicroplasticAsinLookup', () => {
     it('should do stuff', async () => {
-        const microplasticsReader = new MicroplasticAsinLookup();
-        const result = await microplasticsReader.lookup('B00COKKPJK');
+        const result = await MicroplasticAsinLookup.lookup('B00COKKPJK');
 
         expect(result).toBe('Polyethylene');
     });
